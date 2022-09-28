b4267ad6618c47b3e091a13ecd18f8cfe295386a
zeim839 <michael@zeipekkis.com>
2022-09-28 18:49:21 -0400

    feat: ejs view renderer

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { page: 'index' })
+})
+
+app.get('/:page', (req, res) => {
+  res.render('index', { page: req.params.page })
 })
 
 http.listen(3000, () => console.log('Server running on port 3000'))
