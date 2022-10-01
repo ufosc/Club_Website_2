49d5db7ebd2020d46542562c1c9d0aebf7265645
Dev Patel <devpatel2556@gmail.com>
2022-10-01 17:13:36 -0400

    feat: added rate limiter

--- config.js
+++ config.js
@@ -1,14 +1,38 @@
 const NODE_ENV = process.env.NODE_ENV || 'development'
 
+// Rate limiter
+const limiterTemplate = {
+  windowMs: 0,
+  max: 0,
+  message: '401: Too many requests.',
+  standardHeaders: true,
+  legacyHeaders: false
+}
+
 const config = {
   development: {
-    port: process.env.SERVER_PORT || 3000
+    port: process.env.SERVER_PORT || 3000,
+    limiter: {
+      ...limiterTemplate,
+      windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 0,
+      max: process.env.RATE_LIMIT_MAX || 1500
+    }
   },
   staging: {
-    port: process.env.SERVER_PORT || 3001
+    port: process.env.SERVER_PORT || 3001,
+    limiter: {
+      ...limiterTemplate,
+      windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 300000,
+      max: process.env.RATE_LIMIT_MAX || 100
+    }
   },
   production: {
-    port: process.env.SERVER_PORT || 3002
+    port: process.env.SERVER_PORT || 3002,
+    limiter: {
+      ...limiterTemplate,
+      windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 300000,
+      max: process.env.RATE_LIMIT_MAX || 100
+    }
   }
 }
 
