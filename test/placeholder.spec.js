00d75fc3f6178b49e5d248006687d428a98b1e7a
Daniel Wildsmith <danielwildsmith@ufl.edu>
2022-10-20 17:53:05 -0400

    refactor: add test folder with placeholder file

---
+++ test/placeholder.spec.js
@@ -0,0 +1,14 @@
+// Sample Testing File
+/*
+const expect = chai.expect;
+ 
+describe('template', function() {
+    describe('clearCompletedButton', function() {
+        it('should return "Clear completed" when more than 0 items are completed', function() {
+            var template = new app.Template();
+            var result = template.clearCompletedButton(2);
+            expect(result).to.equal('Clear completed');
+        });
+    });
+});
+*/
