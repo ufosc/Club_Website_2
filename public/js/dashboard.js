a542a8b8e4beb19aae05e6067d9bd8d7874dc21c
zeim839 <michael@zeipekkis.com>
2022-10-22 13:02:58 -0400

    refactor: edited comments, change code syntax

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,19 +11,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // adds individual row to selections array
 function checkboxClickHandler (id, tableId, selectionsArray) { // eslint-disable-line
-  const headerCheckboxInputElement = document.getElementById(tableId).getElementsByTagName('thead')[0].getElementsByTagName('input')[0]
-  const rowsArray = document.getElementById(tableId).getElementsByTagName('tbody')[0].getElementsByClassName('data-row')
+  const headerCheckboxInputElement = document.getElementById(tableId)
+    .getElementsByTagName('thead')[0]
+    .getElementsByTagName('input')[0]
+
+  const rowsArray = document.getElementById(tableId)
+    .getElementsByTagName('tbody')[0]
+    .getElementsByClassName('data-row')
+
   const idIndex = selectionsArray.indexOf(id)
+
+  // Element already in array, uncheck it.
   if (idIndex !== -1) {
-    // if all rows are selected set header checkbox to unchecked
     headerCheckboxInputElement.checked = false
-    // remove id from selections array
     selectionsArray.splice(idIndex, 1)
-  } else {
-    // add id to selections array
-    selectionsArray.push(id)
-    // if all rows are selected set header checkbox to checked
-    if (rowsArray.length === selectionsArray.length) { headerCheckboxInputElement.checked = true }
+    return
+  }
+
+  // Element not in array, check it.
+  selectionsArray.push(id)
+  if (rowsArray.length === selectionsArray.length) {
+    headerCheckboxInputElement.checked = true
   }
 }
 
@@ -32,10 +40,9 @@ function selectAllCheckboxClickHandler (tableBodyId, selectionsArray) { // eslin
   const tbodyElement = document.getElementById(tableBodyId)
   const rowsArray = tbodyElement.querySelectorAll('.data-row')
 
-  // if all are selected: remove all
+  // if all are selected: remove and uncheck all
   if (selectionsArray.length === rowsArray.length) {
     selectionsArray.splice(0, selectionsArray.length)
-    // set checkboxes to unchecked
     for (let i = 0; i < rowsArray.length; i++) {
       rowsArray[i].getElementsByTagName('input')[0].checked = false
     }
@@ -45,10 +52,12 @@ function selectAllCheckboxClickHandler (tableBodyId, selectionsArray) { // eslin
   // add all rows that aren't selected
   for (let i = 0; i < rowsArray.length; i++) {
     const rowId = rowsArray[i].id
+
     // don't add row if it's already selected
     if (selectionsArray.indexOf(rowId) !== -1) continue
 
     selectionsArray.push(rowId)
+
     // set checkbox to checked
     rowsArray[i].getElementsByTagName('input')[0].checked = true
   }
