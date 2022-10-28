da9ee8cf1ea9aa1d72f8bf942f791e511e4d82a5
zeim839 <michael@zeipekkis.com>
2022-10-28 12:13:26 -0400

    feat: implemented authentication

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
+require('./auth/passport')
+const bodyParser = require('body-parser')
 const config = require('./config')
 const express = require('express')
+const db = require('./database')
 const rateLimit = require('express-rate-limit')
 const CacheModule = require('./cache')
+const passport = require('passport')
+const auth = require('./auth/auth')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const http = require('http').createServer(app)
@@ -10,9 +17,15 @@ const http = require('http').createServer(app)
 const cache = new CacheModule()
 cache.start(config.cache_interval)
 
-app.use(rateLimit(config.limiter))
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(rateLimit(config.limiter))
 app.use(express.static('public'))
+app.use(express.json())
+app.use(session({ secret: config.secret }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
   res.render('index', { page: 'index' })
@@ -22,12 +35,31 @@ app.get('/preview', (req, res) => {
   res.render('temporary')
 })
 
-app.get(`/${config.admin_route}`, (req, res) => {
-  res.render('login')
+app.post('/auth/login', (req, res) => {
+  passport.authenticate('login', { session: false }, (error, user, info) => {
+    if (error || !user) {
+      return res.status(400).json({ error: info.message })
+    }
+
+    req.login(user, { session: false }, (loginError) => {
+      if (loginError) res.send(loginError)
+      return res.cookie('jwt', auth.tokenizeUser(user)).redirect(`/${config.admin_route}`)
+    })
+  })(req, res)
 })
 
-app.get('/:page', (req, res) => {
+app.get(`/${config.admin_route}`, (req, res) => {
+  passport.authenticate('loggedIn', { session: false }, (error, user, info) => {
+    if (error || !user) {
+      return res.render('login')
+    }
+    return res.render('temporary')
+  })(req, res)
+})
+
+app.get('/:page', passport.authenticate('loggedIn', { session: false }), (req, res) => {
   res.render('index', { page: req.params.page })
 })
 
+db.connect()
 http.listen(config.port, () => console.log(`Server running on port ${config.port}`))
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const NODE_ENV = process.env.NODE_ENV || 'development'
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://dev:Vm0p67n9M0Z9j0jx@cluster0.cfrfrnu.mongodb.net/?retryWrites=true&w=majority'
 
 // Rate limiter
 const limiterTemplate = {
@@ -18,7 +19,8 @@ const config = {
       windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 0,
       max: process.env.RATE_LIMIT_MAX || 1500
     },
-    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 10 * 1000
+    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 10 * 1000,
+    secret: 'jLJhDQMYtXjCwQmu'
   },
   staging: {
     port: process.env.SERVER_PORT || 3001,
@@ -28,7 +30,8 @@ const config = {
       windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 300000,
       max: process.env.RATE_LIMIT_MAX || 100
     },
-    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 15 * 60 * 1000
+    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 15 * 60 * 1000,
+    secret: process.env.SECRET
   },
   production: {
     port: process.env.SERVER_PORT || 3002,
@@ -38,9 +41,11 @@ const config = {
       windowMs: process.env.RATE_LIMIT_TIMEOUT * 60 * 1000 || 300000,
       max: process.env.RATE_LIMIT_MAX || 100
     },
-    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 15 * 60 * 1000
+    cache_interval: process.env.CACHE_INTERVAL * 60 * 1000 || 15 * 60 * 1000,
+    secret: process.env.SECRET
   }
 }
 
 module.exports = config[NODE_ENV]
 module.exports.ENV = NODE_ENV
+module.exports.MONGO_URI = MONGO_URI
