a31544df2debea3dbca7b10a1f1c77891760eb8d
zeim839 <michael@zeipekkis.com>
2022-10-28 12:12:28 -0400

    feat: created users.js

---
+++ model/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    hash: String,
+    salt: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const userModel = mongoose.model('user', userSchema)
+
+module.exports = {
+  userSchema, userModel
+}
