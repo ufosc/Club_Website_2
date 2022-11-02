afd127110926c03e28f4ad49e8e9b8eb33ee8ccc
amitanshusahu <amitanshusahu.official@gmail.com>
2022-11-02 23:28:57 +0530

    fix: linting issues

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,51 @@
-let horizontalScrollContainer = document.querySelector(".horizontal_scroll");
-function Swipe(){
-
-    this.swipeLeft = function(){
-        horizontalScrollContainer.scrollLeft -= 300;
-    }
-    this.swipeRight = function(){
-        horizontalScrollContainer.scrollLeft += 300;
-    }
+const horizontalScrollContainer = document.querySelector('.horizontal_scroll')
+function Swipe () {
+  this.swipeLeft = function () {
+    horizontalScrollContainer.scrollLeft -= 300
+  }
+  this.swipeRight = function () {
+    horizontalScrollContainer.scrollLeft += 300
+  }
 };
 
-let btnLeft = document.querySelector(".btn-left");
-let btnRight = document.querySelector(".btn-right");
-let swipe = new Swipe();
-let scrollableWidth = horizontalScrollContainer.scrollWidth;
-let elementWidth = horizontalScrollContainer.clientWidth;
-let totalScrollableWidth = (scrollableWidth - elementWidth);
+const btnLeft = document.querySelector('.btn-left')
+const btnRight = document.querySelector('.btn-right')
+const swipe = new Swipe()
+const scrollableWidth = horizontalScrollContainer.scrollWidth
+const elementWidth = horizontalScrollContainer.clientWidth
+const totalScrollableWidth = (scrollableWidth - elementWidth)
 
-let leftScroll = totalScrollableWidth;
-let rightScroll = totalScrollableWidth;
+let leftScroll = totalScrollableWidth
+let rightScroll = totalScrollableWidth
 
-btnLeft.onclick = () =>{
-    rightScroll -= 300;
-    if(rightScroll < totalScrollableWidth){
-        btnLeft.style.display = "none";
-    }else{
-        btnLeft.style.display = "block";
-    }
-    swipe.swipeLeft();
-    leftScroll += 300;
-    if(leftScroll < totalScrollableWidth){
-        btnRight.style.display = "none";
-    }else{
-        btnRight.style.display = "block";
-    }
+btnLeft.onclick = () => {
+  rightScroll -= 300
+  if (rightScroll < totalScrollableWidth) {
+    btnLeft.style.display = 'none'
+  } else {
+    btnLeft.style.display = 'block'
+  }
+  swipe.swipeLeft()
+  leftScroll += 300
+  if (leftScroll < totalScrollableWidth) {
+    btnRight.style.display = 'none'
+  } else {
+    btnRight.style.display = 'block'
+  }
 }
 btnRight.onclick = () => {
-    btnLeft.style.display = "block";
-    leftScroll -= 300;
-    if(leftScroll < totalScrollableWidth){
-        btnRight.style.display = "none";
-    }else{
-        btnRight.style.display = "block";
-    }
-    swipe.swipeRight();
-    rightScroll += 300;
-    if(rightScroll < totalScrollableWidth){
-        btnLeft.style.display = "none";
-    }else{
-        btnLeft.style.display = "block";
-    }
+  btnLeft.style.display = 'block'
+  leftScroll -= 300
+  if (leftScroll < totalScrollableWidth) {
+    btnRight.style.display = 'none'
+  } else {
+    btnRight.style.display = 'block'
+  }
+  swipe.swipeRight()
+  rightScroll += 300
+  if (rightScroll < totalScrollableWidth) {
+    btnLeft.style.display = 'none'
+  } else {
+    btnLeft.style.display = 'block'
+  }
 }
