45f06d7c8da78397e393546297e18bcde0d43672
Dev Patel <devpatel2556@gmail.com>
2022-11-10 15:37:53 -0500

    feat: created blog route

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const usersRoute = require('./users')
+const blogRoute = require('./blog')
 const passport = require('passport')
 
 router.use('/users', passport.authenticate('loggedIn', { session: false }), usersRoute)
+router.use('/blog', blogRoute)
 
 module.exports = router
