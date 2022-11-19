873d60d36dffe6974a873dad1982475a3f574132
Dev Patel <devpatel2556@gmail.com>
2022-11-18 20:05:56 -0500

    feat: created callbacks.js

---
+++ callbacks.js
@@ -0,0 +1,19 @@
+const ejs = require('ejs')
+
+const cacheIndexPage = () => {
+  const data = { page: 'UF OSC | Home' }
+  const indexPageData = { indexPage: '' }
+  ejs.renderFile('./views/index.ejs', data, { async: false }, (err, str) => {
+    if (err) {
+      indexPageData.indexPage = '500 INTERNAL SERVER ERROR'
+      return
+    }
+    indexPageData.indexPage = str
+  })
+
+  return indexPageData
+}
+
+module.exports = {
+  callbacks: [cacheIndexPage]
+}
