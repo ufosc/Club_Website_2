56c177d442cf180b1c0bc7a529c6c8c43338a50f
zeim839 <zeim839@users.noreply.github.com>
2023-01-16 12:50:53 -0500

    feat: created contact form API route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+
 const usersRoute = require('./users')
 const blogRoute = require('./blog')
-const passport = require('passport')
+const contactRoute = require('./contact')
 
 router.use('/users', passport.authenticate('loggedIn', { session: false }), usersRoute)
 router.use('/blog', blogRoute)
+router.use('/contact', contactRoute)
 
 module.exports = router
