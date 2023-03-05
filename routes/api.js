071c399dd31b62d4a43d4f6d5dc63972825e8bdd
zeim839 <zeim839@users.noreply.github.com>
2023-03-05 13:03:02 -0500

    feat: created edit route

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,10 @@ const passport = require('passport')
 const usersRoute = require('./users')
 const blogRoute = require('./blog')
 const contactRoute = require('./contact')
+const editRoute = require('./edit')
 
 router.use('/users', passport.authenticate('loggedIn', { session: false }), usersRoute)
+router.use('/edit', passport.authenticate('loggedIn', { session: false }), editRoute)
 router.use('/blog', blogRoute)
 router.use('/contact', contactRoute)
 
