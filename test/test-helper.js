6e2625b548c0030910d581421f73230a59f92804
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-03-23 19:01:38 -0400

    feat: created test helper

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,49 +1,43 @@
-exports.module = class {
-    constructor(chai, app, userToken = null) {
-      this.chai = chai;
-      this.app = app;
-      this.userToken = userToken;
-    }
-  
-    async get(url) {
-      return this.chai.request(this.app)
-        .get(url)
-        .auth(this.userToken, { type: 'bearer' });
-    }
-  
-    async post(url, body = {}) {
-      if (!this.userToken) {
-        return this.chai.request(this.app)
-          .post(url)
-          .send(body);
-      }
-  
-      return this.chai.request(this.app)
-        .post(url)
-        .auth(this.userToken, { type: 'bearer' })
-        .send(body);
-    }
-  
-    async put(url, body = {}) {
-      return this.chai.request(this.app)
-        .put(url)
-        .auth(this.userToken, { type: 'bearer' })
-        .send(body);
-    }
-  
-    async patch(url, body = {}) {
-      return this.chai.request(this.app)
-        .patch(url)
-        .auth(this.userToken, { type: 'bearer' })
-        .send(body);
-    }
-  
-    async delete(url) {
-      return this.chai.request(this.app)
-        .delete(url)
-        .auth(this.userToken, { type: 'bearer' })
-        .send();
-    }
-  };
+class TestHelper {
+  constructor (chai, app, userToken = null) {
+    this.chai = chai
+    this.app = app
+    this.userToken = userToken
+  }
 
-  
+  async get (url) {
+    return this.chai.request(this.app)
+      .get(url)
+      .set('Cookie', `jwt=${this.userToken}`)
+  }
+
+  async post (url, body = {}) {
+    return this.chai.request(this.app)
+      .post(url)
+      .set('Cookie', `jwt=${this.userToken}`)
+      .send(body)
+  }
+
+  async put (url, body = {}) {
+    return this.chai.request(this.app)
+      .put(url)
+      .set('Cookie', `jwt=${this.userToken}`)
+      .send(body)
+  }
+
+  async patch (url, body = {}) {
+    return this.chai.request(this.app)
+      .patch(url)
+      .set('Cookie', `jwt=${this.userToken}`)
+      .send(body)
+  }
+
+  async delete (url) {
+    return this.chai.request(this.app)
+      .delete(url)
+      .set('Cookie', `jwt=${this.userToken}`)
+      .send()
+  }
+}
+
+module.exports = TestHelper
