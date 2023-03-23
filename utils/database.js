aac86db7f3cf8cce872a595db9080d96239a0b4c
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-03-23 18:59:35 -0400

    feat: added disconnect function

--- utils/database.js
+++ utils/database.js
@@ -14,14 +14,22 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose default connection disconnected')
 })
 
-process.on('SIGINT', (error, data) => {
-  console.log(`Disconnected by ${error} ${data}`)
+const __disconnect = () => {
   mongoose.connection.close(() => {
     console.log('Mongoose default connection disconnected through app termination')
     process.exit(0)
   })
+}
+
+process.on('SIGINT', (error, data) => {
+  console.log(`Disconnected by ${error} ${data}`)
+  __disconnect()
 })
 
 exports.connect = () => {
   mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+exports.disconnect = () => {
+  __disconnect()
 }
