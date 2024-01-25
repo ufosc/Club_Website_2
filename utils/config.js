ca970ac3ed935025a67b26c10bd2a37f12f81684
zeim839 <zeim839@users.noreply.github.com>
2024-01-25 16:20:33 -0500

    BUMP V1.0.2

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const VERSION = 'V1.0.1'
+const VERSION = 'V1.0.2'
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://dev:Vm0p67n9M0Z9j0jx@cluster0.cfrfrnu.mongodb.net/?retryWrites=true&w=majority'
 
