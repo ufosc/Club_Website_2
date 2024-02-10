ba5ff5a550be98dd5921e595271afc284f6a6ac5
zeim839 <zeim839@users.noreply.github.com>
2024-02-10 13:15:42 -0500

    feat: render critical styles inline

--- utils/callbacks.js
+++ utils/callbacks.js
@@ -1,10 +1,16 @@
 const ejs = require('ejs')
 const config = require('./config')
+const fs = require('node:fs')
 const { BlogModel } = require('../model/blog')
 
 const cacheIndexPage = async () => {
   const blog = await BlogModel.find({ status: 'published', id: -1 }, null, { limit: 3 }).sort({ date: -1 })
-  const data = { version: config.VERSION, blog }
+
+  // Load critical styles.
+  let styles = fs.readFileSync('./public/css/common.css', 'utf8')
+  styles += fs.readFileSync('./public/css/index.css', 'utf8')
+
+  const data = { version: config.VERSION, blog, styles }
   const indexPageData = { indexPage: '' }
   ejs.renderFile('./views/index.ejs', data, { async: false }, (err, str) => {
     if (err) {
