7a4fa3ae4f8f67653623b484c3b2e1ef6ad06f5c
zeim839 <zeim839@users.noreply.github.com>
2024-05-28 22:53:30 -0400

    feat: create layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import "../global.css"
+import React from "react"
+
+import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
+
+export default function (props: { children: any }) {
+  return (
+    <>
+      <Navbar />
+      { props.children }
+      <Footer />
+    </>
+  )
+}
