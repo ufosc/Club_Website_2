ac7222c2bc7b415c71e2bf2daad8a43f72bf9da7
zeim839 <zeim839@users.noreply.github.com>
2024-05-31 14:43:38 -0400

    feat: configure project data fetching

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,8 +1,7 @@
 import path from 'path'
 import { createFilePath } from 'gatsby-source-filesystem'
 
-export const createPages = async({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+const createBlog = async (createPage: Function, reporter: any, graphql: Function) => {
 
   // Define the template for blog post.
   const blogPost = path.resolve('./src/templates/BlogPostTemplate.tsx')
@@ -52,4 +51,61 @@ export const createPages = async({ graphql, actions, reporter }) => {
       },
     })
   })
+}
+
+const createProjects = async (createPage: Function, reporter: any, graphql: Function) => {
+
+  // Define the template for blog post.
+  const projectTemplate = path.resolve('./src/templates/ProjectTemplate.tsx')
+
+  // Get all markdown blog posts sorted by date
+  const result = await graphql(`
+    {
+      allMdx(sort: { frontmatter: { date: ASC } }, limit: 1000) {
+        nodes {
+          id
+          frontmatter {
+            slug
+          }
+          internal {
+            contentFilePath
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your projects`,
+      result.errors
+    )
+    return
+  }
+
+  const projects = result.data.allMdx.nodes
+  if (projects.length <= 0) {
+    return
+  }
+
+  projects.forEach((project, index) => {
+    const previousProjectId = index === 0 ? null : projects[index - 1].id
+    const nextProjectId = index === projects.length - 1 ? null : projects[index + 1].id
+
+    createPage({
+      path: `/projects${project.frontmatter.slug}`,
+      component: `${projectTemplate}?__contentFilePath=${project.internal.contentFilePath}`,
+      context: {
+        id: project.id,
+        previousProjectId,
+        nextProjectId,
+      },
+    })
+  })
+}
+
+export const createPages = async({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  await createBlog(createPage, reporter, graphql)
+  await createProjects(createPage, reporter, graphql)
 }
