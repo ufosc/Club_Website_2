81aa3f107376db389534a02d9a3d0f4181458aca
zeim839 <zeim839@users.noreply.github.com>
2024-06-02 15:25:44 -0400

    feat: create accordion

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,37 @@
+import './Accordion.css'
+import '../../global.css'
+
+import React, { useState }  from "react"
+import { ChevronDown } from "lucide-react"
+
+export const Accordion = (props: { children?: any }) => (
+  <div className='accordion'>
+    { props.children }
+  </div>
+)
+
+export const AccordionItem = (props: { children?: any, prompt?: string }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const chevronStyle = () => {
+    const anim = (isOpen) ? "flip-open" : "flip-close"
+    return {
+      animation: `0.2s ease-out ${anim} forwards`,
+      color: "white"
+    }
+  }
+
+  return (
+    <div className='accordion-item'>
+      <div className='accordion-item--trigger' onClick={() => setIsOpen(!isOpen)}>
+        <h3>{props.prompt}</h3>
+        <ChevronDown style={chevronStyle()} />
+      </div>
+      {
+        (isOpen) ? (
+          <div className='accordion-item--body'> {props.children} </div>
+        ) : null
+      }
+      <hr />
+    </div>
+  )
+}
