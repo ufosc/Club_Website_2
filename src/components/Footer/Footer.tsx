2c708a86f8bbe3444f92ef4585c6680222bb6c77
zeim839 <zeim839@users.noreply.github.com>
2024-06-05 19:12:36 -0400

    fix: use Gatsby navigation API

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import "./Footer.css"
 import "../../global.css"
 
 import React from "react"
+import { Link } from "gatsby"
 import { version } from "../../../package.json"
 
 import Discord from "../../images/Discord.png"
@@ -15,8 +16,8 @@ export default function () {
       <div>
         <div className="footer-content-container">
           <div className='footer-upper'>
-            <div>
-              <h1 style={{paddingLeft: "20px", fontSize: "18px", float: "left"}}>
+            <div className='footer-upper__text'>
+              <h1 style={{paddingLeft: "20px", fontSize: "18px"}}>
                 UF OSC
               </h1>
               <p className="version-txt">
@@ -24,18 +25,18 @@ export default function () {
               </p>
             </div>
             <div className="social-buttons">
-              <a href="https://discord.gg/Gsxej6u" target="_blank">
+              <Link to="https://discord.gg/Gsxej6u" target="_blank">
                 <img src={Discord} alt="discord" width="35" />
-              </a>
-              <a href="https://github.com/ufosc" target="_blank">
+              </Link>
+              <Link to="https://github.com/ufosc" target="_blank">
                 <img src={Github} alt="github" width="35" />
-              </a>
-              <a href="https://instagram.com/uf_osc?igshid=YmMyMTA2M2Y=" target="_blank">
+              </Link>
+              <Link to="https://instagram.com/uf_osc?igshid=YmMyMTA2M2Y=" target="_blank">
                 <img src={Instagram} alt="instagram" width="35" loading="lazy" />
-              </a>
-              <a href="https://www.facebook.com/groups/ufosc/" target="_blank">
+              </Link>
+              <Link to="https://www.facebook.com/groups/ufosc/" target="_blank">
                 <img src={Facebook} alt="facebook" width="35" loading="lazy" />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -45,7 +46,7 @@ export default function () {
               Copyright (C) { new Date().getFullYear() } Open Source Club
             </h4>
             <div style={{float: "right"}}>
-              <a href="https://github.com/ufosc/Club_Website_2/blob/main/LICENSE.md"
+              <Link to="https://github.com/ufosc/Club_Website_2/blob/main/LICENSE.md"
                 target="_blank"
                 style={{
                   float: "left",
@@ -53,8 +54,8 @@ export default function () {
                   fontWeight: "bold",
                   textDecoration: "underline"
                 }}>
-                License AGPL -3.0-or-later
-              </a>
+                License AGPL-3.0-or-later
+              </Link>
             </div>
           </div>
         </div>
