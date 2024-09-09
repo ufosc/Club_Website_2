e518e9802444c59a07ce089345e3c4b2091f8ac5
zeim839 <zeim839@users.noreply.github.com>
2024-09-09 11:46:58 -0400

    feat: add tracking pixel

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,14 @@ import "../global.css"
 import React from "react"
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
+import TrackingPixel from './TrackingPixel'
 
 const Layout = (props: { children: any }) => (
   <>
     <Navbar />
     { props.children }
     <Footer />
+    <TrackingPixel />
   </>
 )
 
